f229ad26592df7e4872fe120374ec3ca102d919d
Apostolos Smyrnakis <apostolos.smyrnakis@cern.ch>
2021-04-15 20:40:01 +0200

    debug argument + fixes

--- src/thingspeak.py
+++ src/thingspeak.py
@@ -3,6 +3,7 @@
 import sys
 import time
 import urllib
+import argparse
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 
@@ -16,11 +17,16 @@ key = "XXXXXXXXXXXXXXXX"    # Thingspeak API write key
 previousTemperature = 100
 previousHumidity = 100
 
-showDebugs = False
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', action='store_true')
+all_args = parser.parse_args()
+# print(all_args.d)
+
+args = vars(all_args.parse_args())
 
 
 def writeDataFile(humidity, temperature):
-    # Writing last measured values (humidity & temperature) to files for further usage by other scripts
+    """Writting last measured values in files."""
 
     try:
         fh = open('/home/{YOUR-USERNAME}/Software/thingspeak/humidity','wt')
@@ -30,21 +36,25 @@ def writeDataFile(humidity, temperature):
         ft = open('/home/{YOUR-USERNAME}/Software/thingspeak/temperature','wt')
         ft.write(str(temperature))
         ft.close()
-        if showDebugs: print("[THINKGSPEAK] files written")
+        if all_args.d: print("[THINKGSPEAK] files written")
     except:
         print("[THINKGSPEAK] Failed to write files !")
 
 
 def validateDHT(humidity, temperature):
-    # DHT11 range is 0-50 degrees C for temperature and 20-90 % for humidity
-    # This function validates that:
-    #   - the numbers are inside the above ranges
-    #   - current measurement is no more than 5 numbers different than the previous measurement
-    #     If |current - previous| > 5 : re-measure for up to 4 times every 2 sec
-    #
-    # The above validations are needed for two reasons:
-    #   - DHT11 tends to be a bit unstable and easily affected by environment variations
-    #   - in my setup, the DHT11's cable is approx 1.5m long. This is not recommended!
+    """Validating DHT11 values.
+
+    Temperature range:  0-50 degrees C
+    Humidity range:     20-90 %
+    This function validates that:
+      - the numbers are inside the above ranges
+      - current measurement is no more than 5 numbers different than the previous measurement
+        If |current - previous| > 5 : re-measure for up to 4 times every 2 sec
+
+    The above validations are needed for two reasons:
+      - DHT11 tends to be a bit unstable and easily affected by environment variations
+      - in my setup, the DHT11's cable is approx 1.5m long. This is not recommended!
+    """
 
     global previousTemperature
     global previousHumidity
@@ -57,7 +67,7 @@ def validateDHT(humidity, temperature):
         time.sleep(2)
 
         humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
-        if showDebugs: print("[THINKGSPEAK] RE-MEASUREMENT T:",temperature,"H:",humidity,"whileCounter:",whileCounter)
+        if all_args.d: print("[THINKGSPEAK] RE-MEASUREMENT T:",temperature,"H:",humidity,"whileCounter:",whileCounter)
 
     previousHumidity = humidity
     previousTemperature = temperature
@@ -67,34 +77,36 @@ def validateDHT(humidity, temperature):
 
 
 def measure_send():
+    """Sending values to ThingSpeak."""
+
     global previousTemperature
     global previousHumidity
 
     # get CPU temperature
     tempCPU = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3
-    if showDebugs: print("[THINKGSPEAK] CPU temperature:",tempCPU)
+    if all_args.d: print("[THINKGSPEAK] CPU temperature:",tempCPU)
 
     # get load averages
     loadAll = open('/proc/loadavg').read()
     load1 = loadAll[0:4]
     load5 = loadAll[5:9]
     load15 = loadAll[10:14]
-    if showDebugs: print("[THINKGSPEAK] Average-1:",load1, "Average-5:",load5, "Average-15:",load15)
+    if all_args.d: print("[THINKGSPEAK] Average-1:",load1, "Average-5:",load5, "Average-15:",load15)
 
     try:
         # get room temperature & humidity
         humRoom, tempRoom = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
-        if showDebugs:
+        if all_args.d:
             print("[THINKGSPEAK] FIRST measuremet T:",tempRoom,"H:",humRoom)
             print("[THINKGSPEAK] previousTemperature:",previousTemperature,"previousHumidity:",previousHumidity)
 
         if previousHumidity == 100 or previousTemperature == 100 :
-            if showDebugs: print("[THINKGSPEAK] initialising 'previous' values")
+            if all_args.d: print("[THINKGSPEAK] initialising 'previous' values")
             previousHumidity = humRoom
             previousTemperature = tempRoom
 
         humidityRoom, temperatureRoom = validateDHT(humRoom, tempRoom)
-        if showDebugs: print("[THINKGSPEAK] >>>> VALIDATED measurement T:",temperatureRoom, "H:",humidityRoom)
+        if all_args.d: print("[THINKGSPEAK] >>>> VALIDATED measurement T:",temperatureRoom, "H:",humidityRoom)
 
         # Parameters WITH DHT11
         if sys.version_info[0] < 3:
@@ -121,11 +133,11 @@ def measure_send():
     try:
         conn.request("POST", "/update", params, headers)
         response = conn.getresponse()
-        if showDebugs: print("[THINKGSPEAK] connection to Thingspeak:", response.status, response.reason)
-        data = response.read()
+        if all_args.d: print("[THINKGSPEAK] connection to Thingspeak:", response.status, response.reason)
+        # data = response.read()
         conn.close()
     except:
-        if showDebugs: print("[THINKGSPEAK] connection to Thingspeak failed !")
+        if all_args.d: print("[THINKGSPEAK] connection to Thingspeak failed !")
 
 
 if __name__ == "__main__":
