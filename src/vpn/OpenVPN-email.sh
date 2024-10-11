cdabd5e20df6f29623fa520bda874fb0210c0e20
Apostolos Smyrnakis <smyrnakis@users.noreply.github.com>
2024-10-12 00:30:35 +0200

    Update OpenVPN-email.sh

--- src/vpn/OpenVPN-email.sh
+++ src/vpn/OpenVPN-email.sh
@@ -2,10 +2,10 @@
 
 # This script is monitoring OpenVPN log ( /var/log/openvpn-status.log )
 # and notify via email on client's connection / disconnection.
-# Replace {YOUR-EMAIL} on 'recipient' variable (line 13).
+# Replace {YOUR-EMAIL} on 'recipient' variable (line 14).
 #
 # 2021/02/01 - Apostolos Smyrnakis
-# Updated 2023/03/13
+# Updated 2024/10/11
 # https://github.com/smyrnakis/raspberry-born/blob/main/src/vpn/OpenVPN-email.sh
 
 echo "Starting script 'OpenVPN-email.sh' to monitor openvpn.log file ..."
@@ -20,8 +20,13 @@ flagDisconnected=1
 # check openvpn log for connected clients
 operation ()
 {
+    if [[ "$recipient" == "{YOUR-EMAIL}" ]]; then
+        echo "Error: Please replace '{YOUR-EMAIL}' with the recipient's email address."
+        exit 1
+    fi
+
     # extract 'client name' , 'source IP' , 'connected since'
-    logExtract=$( cat /var/log/openvpn-status.log | tail -n +4 | grep 'CLIENT_LIST' | awk -F ',' '{print $2,$3,$8}' )
+    logExtract=$( tail -n +4 /var/log/openvpn-status.log | grep 'CLIENT_LIST' | awk -F ',' '{print $2,$3,$8}' )
 
     if [[ ! -z "$logExtract" ]]; then
         if [[ "$oldConnected" != "$logExtract" ]]; then
@@ -36,14 +41,18 @@ operation ()
                 emailMessage+="\t-->  '${connectedDetails[0]}'   from   ${connectedDetails[1]}   since   ${connectedDetails[3]}, ${connectedDetails[2]}\r\n "
             done
 
-            echo -e "Subject: OpenVPN on ${hostn}\r\n\r\nCurrently connected client(s):\r\n\r\n${emailMessage}\r\n" | /usr/bin/msmtp --from=default --syslog=on -t ${recipient}
+            if ! echo -e "Subject: OpenVPN on ${hostn}\r\n\r\nCurrently connected client(s):\r\n\r\n${emailMessage}\r\n" | /usr/bin/msmtp --from=default --syslog=on -t ${recipient}; then
+                echo "Error: Failed to send email notification for connected clients."
+            fi
             )
         fi
     else
         if [[ "$flagDisconnected" -ne 1 ]]; then
             flagDisconnected=1
 
-            echo -e "Subject: OpenVPN on ${hostn}\r\n\r\nAll clients DISCONNECTED\r\n\r\n" | /usr/bin/msmtp --from=default --syslog=on -t ${recipient}
+            if ! echo -e "Subject: OpenVPN on ${hostn}\r\n\r\nAll clients DISCONNECTED\r\n\r\n" | /usr/bin/msmtp --from=default --syslog=on -t ${recipient}; then
+                echo "Error: Failed to send email notification for disconnected clients."
+            fi
         fi
     fi
 }
